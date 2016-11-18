0b0f84ea1800c37e5c8e99b9d6ac9cef2f4d788b
Philip Del Vecchio <pdv@gc.com>
2016-11-18 12:19:31 -0500

    Load piano sounds

--- fase.js
+++ fase.js
@@ -7,6 +7,8 @@ const ctx = canvas.getContext('2d');
 const cw = canvas.width;
 const ch = canvas.height;
 
+const actx = new AudioContext();
+
 const SEQUENCE_LENGTH = 12;
 const NUM_NOTES = 12;
 const SIDE_LENGTH = ch / NUM_NOTES;
@@ -14,17 +16,7 @@ const SIDE_LENGTH = ch / NUM_NOTES;
 // 55x55 squres
 
 let notes;
-
-canvas.addEventListener('click', (e) => {
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    const column = Math.floor(x / SIDE_LENGTH);
-    const row = Math.floor(y / SIDE_LENGTH);
-    if (column > 0) {
-        onRectClicked(column - 1, row);
-    }
-});
+let pianoBuffer = [];
 
 function makeNotes() {
     notes = [];
@@ -36,6 +28,10 @@ function makeNotes() {
     }
     return notes;
 }
+
+
+////////////////////////////////////////////////// Root draw
+
 
 function drawGrid() {
     ctx.strokeStyle = 'black';
@@ -70,6 +66,10 @@ function drawPiano(root) {
     }
 }
 
+
+////////////////////////////////////////////////// Click handling
+
+
 function fillBox(column, row, color) {
     const x = (column + 1) * SIDE_LENGTH + 1;
     const y = row * SIDE_LENGTH + 1;
@@ -86,6 +86,17 @@ function colorBox(column, row) {
     fillBox(column, row, color);
 }
 
+canvas.addEventListener('click', (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const column = Math.floor(x / SIDE_LENGTH);
+    const row = Math.floor(y / SIDE_LENGTH);
+    if (column > 0) {
+        onRectClicked(column - 1, row);
+    }
+});
+
 function onRectClicked(column, row) {
 
     if (notes[column][row] == 0) {
@@ -97,7 +108,53 @@ function onRectClicked(column, row) {
     }
 }
 
+
+////////////////////////////////////////////////// Sound
+
+function loadNote(note, url) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.onload = () => {
+        actx.decodeAudioData(
+            request.response,
+            (buffer) => pianoBuffer[note] = buffer,
+            (e) => console.log("Error loading sound")
+        );
+    };
+    request.send();
+}
+
+function loadPiano() {
+    for (let i = 1; i <= 64; i++) {
+        const fileName = 'piano/' + i + '.wav';
+        loadNote(i + 20, fileName);
+    }
+}
+
+function playNote(note, delay) {
+    let source = actx.createBufferSource();
+    source.buffer = pianoBuffer[note];
+    source.connect(actx.destination);
+    source.start(delay);
+}
+
+function play() {
+    playNote(60, 0);
+    const colTime = 200;
+    for (var column; column < SEQUENCE_LENGTH; column++) {
+        for (var row; row < NUM_NOTES; row++) {
+            if (notes[column][row] == 1) {
+                playNote(NUM_NOTES - row, column * colTime);
+            }
+        }
+    }
+}
+
+$('play').addEventListener('click', play);
+
 function main() {
+    loadPiano();
     makeNotes();
     drawGrid();
     drawPiano(60);
